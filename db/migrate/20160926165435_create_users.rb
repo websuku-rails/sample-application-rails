c903ebf06e6c4f23548bc00320d5bd25d57e0b78
yuya0206 <okumura.infratop@gmail.com>
2016-10-16 09:51:34 +0000

    remove migration

--- db/migrate/20160926165435_create_users.rb
+++ db/migrate/20160926165435_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :user_id
+      t.integer :user_id
 
       t.timestamps
     end
--- db/migrate/20161009104511_change_user_id_of_users.rb
+++
@@ -1,5 +0,0 @@
-class ChangeUserIdOfUsers < ActiveRecord::Migration[5.0]
-  def change
-    change_column :users, :user_id, :integer
-  end
-end
